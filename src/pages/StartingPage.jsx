9c818a0fa53700aff2cac74015d91a15e7d67a5d
Billy Arredondo <billy.arredondo@pucp.pe>
2022-08-26 05:26:58 -0500

    Starting Page with localStorage

--- src/pages/StartingPage.jsx
+++ src/pages/StartingPage.jsx
@@ -1,28 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useForm } from "../hooks/useForm";
 
 export const StartingPage = () => {
 
+    const inputRef = useRef();
+
     const {
         username = "",
-        onInputChange
+        onInputChange,
+        setFieldState
     } = useForm({
         username: ""
     });
+
+    useEffect(() => {
+        setFieldState({ name: "username", value: localStorage.getItem("username") ?? "" });
+        inputRef.current.select();
+    }, []);
 
     const isValidUser = () => username?.length >= 3;
 
     const handleClick = (e) => {
         e.preventDefault();
-        console.log(username);
+        localStorage.setItem("username", username?.toLowerCase());
     }
 
     return (
         <div className="position-absolute top-50 start-50 translate-middle col-10 col-md-4">
             <form className="text-center">
                 <input
+                    ref={inputRef}
                     type="text"
-                    className="form-control text-center mt-3"
+                    className="form-control text-center mt-3 text-lowercase"
                     placeholder="Your name here"
                     name="username"
                     value={username}
