b8ff81db109989f9e332966de3491e4c6590744f
Billy Arredondo <billy.arredondo@pucp.pe>
2022-08-26 04:30:43 -0500

    Implement StartingPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { StartingPage } from "./pages/StartingPage";
 function App() {
     return (
         <>
-            <h1>Multiplicolas</h1>
-            <Routes>
-                <Route path="/" element={<StartingPage />} />
-            </Routes>
+            <div className="m-3">
+                <h1>Multiplicolas</h1>
+                <Routes>
+                    <Route path="/" element={<StartingPage />} />
+                </Routes>
+            </div>
         </>
     );
 }
--- src/pages/StartingPage.jsx
+++ src/pages/StartingPage.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 export const StartingPage = () => {
     return (
-        <div>StartingPage</div>
+        <div className="position-absolute top-50 start-50 translate-middle col-10 col-md-4">
+            <form className="text-center">
+                <input type="text" className="form-control text-center mt-3" placeholder="Your name here" />
+                <button className="btn btn-outline-primary mt-3 col-12">
+                    Go
+                </button>
+            </form>
+        </div>
     );
 }
