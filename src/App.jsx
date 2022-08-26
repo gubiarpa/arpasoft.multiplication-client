700ac4fe7bcc92e826c8267579251a4519828c8c
Billy Arredondo <billy.arredondo@pucp.pe>
2022-08-26 04:05:41 -0500

    Install Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { StartingPage } from "./pages/StartingPage";
+
 function App() {
-  return (
-      <>
-          <h1>Multiplicolas</h1>
-      </>
-  );
+    return (
+        <>
+            <h1>Multiplicolas</h1>
+            <Routes>
+                <Route path="/" element={<StartingPage />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/pages/StartingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const StartingPage = () => {
+    return (
+        <div>StartingPage</div>
+    );
+}
