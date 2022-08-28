ae46c501f1b6276bc062711b53d94e53ab6d47ff
Billy Arredondo <billy.arredondo@pucp.pe>
2022-08-28 01:44:17 -0500

    Add Icon on Menu

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftRotate } from "@fortawesome/free-solid-svg-icons";
 
 export const MenuPage = () => {
 
@@ -53,7 +55,7 @@ export const MenuPage = () => {
                     className="btn btn-outline-secondary mt-3 col-12 fs-3"
                     onClick={handleBackClick}
                 >
-                    My Name
+                    <FontAwesomeIcon icon={faArrowLeftRotate} /> Back
                 </button>
             </form>
         </div>
